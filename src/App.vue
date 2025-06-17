8301ed3e5986e37dffe537a326dc4fcf1a6cd94a
280623337 <50629030+280623337@users.noreply.github.com>
2025-06-17 15:27:24 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <el-config-provider namespace="ep">
-    <BaseHeader />
+    <BaseHeader @exam-change="onExamChange" />
     <div class="flex main-container">
       <!-- <BaseSide /> -->
       <div w="full" py="4">
         <!-- <Logos my="4" /> -->
-        <HelloWorld msg="Hello Vue 3 + Element Plus + Vite" />
+        <HelloWorld :examType="examType" msg="Hello Vue 3 + Element Plus + Vite" />
       </div>
     </div>
   </el-config-provider>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const examType = ref('psm');
+function onExamChange(val: string) {
+  examType.value = val;
+}
+</script>
 
 <style>
 #app {
